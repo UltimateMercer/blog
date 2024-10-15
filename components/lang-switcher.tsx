30efd17353537297782946dd4bd5809d09b53e81
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-15 16:31:52 -0300

    feat: added language switcher

---
+++ components/lang-switcher.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import * as React from "react";
+import { useLanguageStore } from "@/store/useLanguageStore";
+
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { Languages } from "lucide-react";
+
+interface LanguageStore {
+  language: "pt-br" | "en-us";
+  setLanguage: (language: string) => void;
+}
+
+export function LangSwitcher() {
+  const { language, setLanguage } = useLanguageStore() as LanguageStore;
+
+  return (
+    <DropdownMenu>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Languages className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all " />
+              </Button>
+            </DropdownMenuTrigger>
+          </TooltipTrigger>
+          <TooltipContent side="left">
+            <p>
+              {language === "en-us" ? "Change Language..." : "Mudar idioma..."}
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <DropdownMenuContent>
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onClick={() => setLanguage("en-us")}
+        >
+          English
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onClick={() => setLanguage("pt-br")}
+        >
+          Português
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
