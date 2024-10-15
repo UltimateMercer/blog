b54b1f319a83cdc279931a5484c93c6b952ebb2d
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-15 11:33:29 -0300

    fix: get articles function

--- services/index.ts
+++ services/index.ts
@@ -2,7 +2,11 @@ import { compareDesc } from "date-fns";
 
 import { allArticles } from "contentlayer/generated";
 
-export const getArticles = () =>
-  allArticles.sort((a: any, b: any) =>
-    compareDesc(new Date(a.date), new Date(b.date))
-  );
+interface ArticleLangProps {
+  lang?: "pt-br" | "en-us";
+}
+
+export const getArticles = ({ lang = "pt-br" }: ArticleLangProps) =>
+  allArticles
+    .filter((article) => article.lang === lang)
+    .sort((a: any, b: any) => compareDesc(new Date(a.date), new Date(b.date)));
