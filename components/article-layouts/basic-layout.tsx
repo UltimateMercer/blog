859349862469781b4a0d14ff442efc74ec835a71
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-16 21:49:55 -0300

    feat: added author space

--- components/article-layouts/basic-layout.tsx
+++ components/article-layouts/basic-layout.tsx
@@ -1,17 +1,21 @@
+"use client";
 import Link from "next/link";
+import Image from "next/image";
 import { FormatFullTimeStamp } from "../date-format";
 import { Badge } from "../ui/badge";
-import type { ArticleLayoutProps } from "@/utils/interfaces";
+import type { ArticleLayoutProps, LanguageStore } from "@/utils/interfaces";
+import { useLanguageStore } from "@/store/useLanguageStore";
 
 const BasicLayout = ({ doc, children }: ArticleLayoutProps) => {
   const { title, tags, date, image = "" } = doc;
+  const { language } = useLanguageStore() as LanguageStore;
 
   return (
     <section className="main-article">
       <div className="header-basic">
         <div className="header-basic-content">
           <h5 className="font-sans font-semibold tracking-wide">
-            <FormatFullTimeStamp date={date} />
+            <FormatFullTimeStamp date={date} locale={language} />
           </h5>
           <h1 className="md:text-4xl text-3xl font-bold tracking-wide mb-4">
             {title}
@@ -40,6 +44,23 @@ const BasicLayout = ({ doc, children }: ArticleLayoutProps) => {
             ))}
           </div>
         )}
+        <div className="my-6">
+          <h3 className="mb-1">Escrito por:</h3>
+          <div className="flex md:flex-row flex-col items-center">
+            <Image
+              src={"https://i.imgur.com/55Kcg9V.jpeg"}
+              className={`w-36 h-36 rounded-full object-cover hover:scale-105 hover:ring-2 ring-gray-300 hover:shadow-lg transition-all md:mb-0 !mt-0 mb-4 `}
+              width={10000}
+              height={10000}
+              alt={`Image`}
+            />
+            <div className="flex flex-col flex-1 justify-center ml-6">
+              <h4 className="text-3xl font-bold tracking-wide mb-2">
+                Julian Silva da Cunha
+              </h4>
+            </div>
+          </div>
+        </div>
       </article>
     </section>
   );
