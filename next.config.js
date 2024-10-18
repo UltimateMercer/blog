3040b9d48b8523c545bedf0ad78682b4739b2a53
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-18 00:40:04 -0300

    chore: change next config

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const { withContentlayer } = require("next-contentlayer");
+// import { withContentlayer } from "next-contentlayer";
+
+/** @type {import('next').NextConfig} */
+
+
+const nextConfig = withContentlayer({
+  basePath: "/blog",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+});
+
+// export default nextConfig;
+// export default withContentlayer(nextConfig);
+module.exports = withContentlayer(nextConfig);
