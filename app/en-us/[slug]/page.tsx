62be0cc4abc858118727365f018923dd57d78ea1
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-18 01:39:46 -0300

    feat: added metadata

--- app/en-us/[slug]/page.tsx
+++ app/en-us/[slug]/page.tsx
@@ -1,12 +1,47 @@
+import { Metadata, ResolvingMetadata } from "next";
 import { MDXComponents } from "@/components/mdx";
 import { getArticles } from "@/services";
 import { notFound } from "next/navigation";
+
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export async function generateStaticParams() {
   const articles = getArticles({ lang: "en-us" });
   return articles.map((article) => ({
     slug: article.slug,
   }));
+}
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const articles = getArticles({ lang: "en-us" });
+  const article = articles.find(
+    (article) => article.slug === "en-us/" + params.slug
+  );
+
+  const image = article?.image ? article.image : "/ultimate-mercer-base.jpg";
+
+  return {
+    title: article?.title,
+    authors: [{ name: "Ultimate Mercer" }, { name: "Julian Silva da Cunha" }],
+    description: article?.description,
+    openGraph: {
+      title: article?.title,
+      description: article?.description,
+      images: image,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: article?.title,
+      description: article?.description,
+      images: image,
+    },
+  };
 }
 
 export default function Page({ params }: { params: { slug: string } }) {
--- app/pt-br/[slug]/page.tsx
+++ app/pt-br/[slug]/page.tsx
@@ -1,13 +1,47 @@
-import { FormatFullTimeStamp } from "@/components/date-format";
+import { Metadata, ResolvingMetadata } from "next";
 import { MDXComponents } from "@/components/mdx";
 import { getArticles } from "@/services";
 import { notFound } from "next/navigation";
+
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export async function generateStaticParams() {
   const articles = getArticles({ lang: "pt-br" });
   return articles.map((article) => ({
     slug: article.slug,
   }));
+}
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const articles = getArticles({ lang: "pt-br" });
+  const article = articles.find(
+    (article) => article.slug === "pt-br/" + params.slug
+  );
+
+  const image = article?.image ? article.image : "/ultimate-mercer-base.jpg";
+
+  return {
+    title: article?.title,
+    authors: [{ name: "Ultimate Mercer" }, { name: "Julian Silva da Cunha" }],
+    description: article?.description,
+    openGraph: {
+      title: article?.title,
+      description: article?.description,
+      images: image,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: article?.title,
+      description: article?.description,
+      images: image,
+    },
+  };
 }
 
 export default function Page({ params }: { params: { slug: string } }) {
