74861236903c2884d2e828e67fd1287d7fd16b5f
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-10-22 22:10:19 -0300

    test(e2e): test access an article, change language and then return to home page

--- cypress/e2e/article.cy.ts
+++ cypress/e2e/article.cy.ts
@@ -3,7 +3,7 @@ import { ar } from "date-fns/locale";
 describe("Articles", () => {
   let initialHref: string = "";
   let articleHref: string = "";
-  const oldArticleHref: string = "";
+  let oldArticleHref: string = "";
 
   beforeEach(() => {
     cy.visit("http://localhost:3000/blog", { timeout: 120000 });
@@ -16,8 +16,6 @@ describe("Articles", () => {
   it("access an article then return to home page", () => {
     cy.location("href").then((href) => {
       initialHref = href;
-      cy.log("#############################################");
-      cy.log(initialHref);
     });
 
     cy.get("#list_articles").should("exist").and("be.visible");
@@ -30,9 +28,6 @@ describe("Articles", () => {
 
     cy.location("href").then((href) => {
       articleHref = href;
-      cy.log("#############################################");
-      cy.log(articleHref);
-
       expect(articleHref).to.not.equal(initialHref);
     });
 
@@ -47,48 +42,42 @@ describe("Articles", () => {
     });
   });
 
-  // it("return to home page", () => {
-  //   cy.location("href").then((href) => {
-  //     initialHref = href;
-  //     cy.log("#############################################");
-  //     cy.log(initialHref);
-  //   });
+  it("access an article, change language and then return to home page", () => {
+    cy.location("href").then((href) => {
+      initialHref = href;
+    });
 
-  //   cy.get("#list_articles").should("exist").and("be.visible");
+    cy.get("#list_articles").should("exist").and("be.visible");
 
-  //   cy.get("#list_articles a").should("have.length.gt", 0);
+    cy.get("#list_articles a").should("have.length.gt", 0);
 
-  //   cy.get("#list_articles a").first().click();
+    cy.get("#list_articles a").last().click();
 
-  //   cy.get("#main_article").should("exist").and("be.visible");
+    cy.get("#main_article").should("exist").and("be.visible");
 
-  //   cy.location("href").then((href) => {
-  //     articleHref = href;
-  //     cy.log("#############################################");
-  //     cy.log(articleHref);
+    cy.location("href").then((href) => {
+      articleHref = href;
+      expect(articleHref).to.not.equal(initialHref);
+    });
 
-  //     expect(articleHref).to.not.equal(initialHref);
-  //   });
+    cy.get("#lang-toggle-trigger").click();
+    cy.get("#lang-toggle-pt").click();
 
-  // cy.location("href").then((href) => {
-  //   oldArticleHref = articleHref;
-  //   articleHref = href;
-  //   cy.log("#############################################");
-  //   cy.log(articleHref);
+    oldArticleHref = articleHref;
 
-  //   expect(articleHref).to.not.equal(oldArticleHref);
-  // });
+    cy.location("href").then((href) => {
+      articleHref = href;
+      expect(articleHref).to.not.equal(oldArticleHref);
+    });
 
-  // cy.get("#lang-toggle-trigger").click();
-  // cy.get("#lang-toggle-pt").click();
-  //   cy.get("#main_article").should("exist").and("be.visible");
+    cy.get("#main_article").should("exist").and("be.visible");
 
-  //   // cy.get("#return_btn").should("exist").and("be.visible");
+    cy.get("#return_btn").should("exist").and("be.visible");
 
-  //   // cy.get("#return_btn").click();
+    cy.get("#return_btn").click();
 
-  //   cy.location("href").then((href) => {
-  //     expect(href).to.eq(initialHref);
-  //   });
-  // });
+    cy.location("href").then((href) => {
+      expect(href).to.eq(initialHref);
+    });
+  });
 });
